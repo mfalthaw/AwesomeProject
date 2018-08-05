08858b8f7cfb20f2202775f969d828afd5416221
mfalthaw <mfalthaw@ucalgary.ca>
2018-08-05 20:13:53 +0300

    layout with flexbox

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      <View style={{
+        flex: 1, 
+        flexDirection: 'row',
+        justifyContent: 'center'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
