2d8d222f5489ed6c6e1c1471bf3337685cdcbca0
mfalthaw <mfalthaw@ucalgary.ca>
2018-08-07 19:37:05 +0300

    Handling touches

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  StyleSheet, Text, TextInput, Button, TouchableHighlight, TouchableOpacity,
+  TouchableWithoutFeedback, Alert, View
+} from 'react-native';
 import React from 'react';
 
 export default class App extends React.Component {
@@ -8,37 +11,57 @@ export default class App extends React.Component {
   }
 
   onChangeText = (text) => this.setState({ text });
+  onPressButton = () => Alert.alert('You pressed the button!');
+  onLongPressButton = () => Alert.alert('You Long-pressed the button!');
 
   render() {
     return (
-      <View style={styles.view}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Type here to translate!"
-          onChangeText={this.onChangeText}
-        />
-        <Text style={styles.text}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this.onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableOpacity onPress={this.onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableWithoutFeedback onPress={this.onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableHighlight onPress={this.onPressButton} onLongPress={this.onLongPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+
       </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  view: {
-    padding: 10,
+  container: {
     flex: 1,
-    flexDirection: 'column',
     justifyContent: 'center',
+    paddingTop: 60,
     alignItems: 'center',
   },
-  textInput: {
-    height: 40,
-    fontSize: 24,
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
   },
-  text: {
-    padding: 10,
-    fontSize: 42,
+  buttonText: {
+    padding: 20,
+    color: 'white',
   },
 });
